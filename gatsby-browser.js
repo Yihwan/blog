4445eeb0a3a1d86c50160e970c8e48abbf8c5320
yihwan <hi@yihwan.kim>
2019-07-06 11:04:47 +0700

    remove browser api

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -12,9 +12,3 @@ export const onClientEntry = () => {
 export const shouldUpdateScroll = args => {
     return false;
 };
-
-export const wrapPageElement = ({ element, props }) => {
-  // props provide same data to Layout as Page element will get
-  // including location, data, etc - you don't need to pass it
-  return <Layout {...props}>{element}</Layout>
-}
